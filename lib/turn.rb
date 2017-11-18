d6bfda45ee7ffc283d0c30b54f1eb8c527384670
PJ Frias <peejfrias@gmail.com>
2017-11-18 21:22:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,3 +7,13 @@
 # else
 #   ask for input again until you get a valid input
 # end
+
+#display_board creates a blank tic tac toe board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+end
