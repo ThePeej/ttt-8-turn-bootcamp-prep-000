f675dcefdb1b693675a918c0780b4bdd10c9e296
PJ Frias <peejfrias@gmail.com>
2017-11-18 21:20:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,10 @@
-/*
-ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
-end
-<<<<<<< HEAD
-*/
-=======
-*/
->>>>>>> 171424150abd68cc978c076f1934712de784e58c
+# ask for input
+# get input
+# convert input to index
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
+
