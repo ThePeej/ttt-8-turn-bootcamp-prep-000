343bac5571d11b1be295bb89fbd4048d8f74c6c7
PJ Frias <peejfrias@gmail.com>
2017-11-18 22:45:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -51,7 +51,7 @@ def turn()
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board,index)
-    move(board, index, player = "X")
+    move(board, index, player)
   else
     turn()
   end
